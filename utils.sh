1cbb6433427d00efbade1c98fa95df7ddcd78f66
avidalto <avidalto@hawk.iit.edu>
2022-04-28 19:16:33 +0000

    Update

--- utils.sh
+++ utils.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ssh_options="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
 is_in_list () {
     list=$1
@@ -31,12 +32,12 @@ ssh_establish_tunnel_to_head_node() {
     HOST_USER=$2
     port_1=$3
     port_2=$4
-    int_ip=$(hostname -I | sed "s/ //g")
-    u=${HOST_USER}
+    int_ip=$(hostname -I | cut -d' ' -f1 | sed "s/ //g")
+    u=${PW_USER}
     s=${PW_USER_HOST}
-    tunnel_cmd="sudo -E -u ${HOST_USER} bash -c \"setsid ssh -L 0.0.0.0:${port_1}:${int_ip}:${port_1} -L 0.0.0.0:${port_2}:${int_ip}:${port_2} ${u}@${s} -fNT\""
+    tunnel_cmd="sudo -E -u ${HOST_USER} bash -c \"setsid ssh ${ssh_options} -L 0.0.0.0:${port_1}:${int_ip}:${port_1} -L 0.0.0.0:${port_2}:${int_ip}:${port_2} ${u}@${s} -fNT\""
     echo ${tunnel_cmd} > establish_tunnel_to_head_node.sh
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${HOST_USER}@${HOST_IP} -t 'sudo bash -s' < establish_tunnel_to_head_node.sh
+    ssh ${ssh_options} ${HOST_USER}@${HOST_IP} -t 'sudo bash -s' < establish_tunnel_to_head_node.sh
 }
 
 ssh_cancel_tunnel_to_head_node() {
@@ -44,11 +45,11 @@ ssh_cancel_tunnel_to_head_node() {
     HOST_USER=$2
     port_1=$3
     port_2=$4
-    int_ip=$(hostname -I | sed "s/ //g")
-    u=${HOST_USER}
+    int_ip=$(hostname -I | cut -d' ' -f1 | sed "s/ //g")
+    u=${PW_USER}
     s=${PW_USER_HOST}
-    tunnel_cmd="sudo -E -u ${PW_USER} bash -c \"ssh -O cancel -L 0.0.0.0:${port_1}:${int_ip}:${port_1} -L 0.0.0.0:${port_2}:${int_ip}:${port_2} ${u}@${s} -fNT\""
+    tunnel_cmd="sudo -E -u ${PW_USER} bash -c \"ssh ${ssh_options} -O cancel -L 0.0.0.0:${port_1}:${int_ip}:${port_1} -L 0.0.0.0:${port_2}:${int_ip}:${port_2} ${u}@${s} -fNT\""
     echo ${tunnel_cmd} > cancel_tunnel_to_head_node.sh
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${HOST_USER}@${HOST_IP} -t 'sudo bash -s' < cancel_tunnel_to_head_node.sh
+    ssh ${ssh_options} ${HOST_USER}@${HOST_IP} -t 'sudo bash -s' < cancel_tunnel_to_head_node.sh
 }
 
