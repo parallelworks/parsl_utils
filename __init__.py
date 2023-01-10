44254889949a323da5881cfa6f9f9df2f19facb2
avidalto <avidalto@hawk.iit.edu>
2023-01-10 11:04:39 -0600

    Add generic config here

--- __init__.py
+++ __init__.py
@@ -6,6 +6,9 @@ from . import popen
 from . import ssh_cmds
 from . import pool_api
 
+if os.path.isfile('executors.json'):
+    from . import config
+
 if 'PARSL_CLIENT_HOST' in os.environ:
     # Only works if we are running parsl_utils in the user container
     from . import resource_info
