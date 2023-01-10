d68f49f8726ca031a166be1b9a194e6a946f3b10
avidalto <avidalto@hawk.iit.edu>
2023-01-10 14:57:16 -0600

    Add gsutil

--- data_provider/__init__.py
+++ data_provider/__init__.py
@@ -1,6 +1,8 @@
 from parsl.data_provider.files import File
 
 from . import rsync
+from . import gsutil
+
 
 def PWFile(path, local_path, scheme, netloc = None, isdir = False):
     # Set isdir = True if the file is a directory!
