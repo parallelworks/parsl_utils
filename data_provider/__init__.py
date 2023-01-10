6d1b5bdc49137be68c624dbfd69e28c9169f8ba5
avidalto <avidalto@hawk.iit.edu>
2023-01-10 16:10:17 -0600

    Attempt to fix File defnition

--- data_provider/__init__.py
+++ data_provider/__init__.py
@@ -6,9 +6,10 @@ from . import gsutil
 
 def PWFile(path, local_path, scheme, netloc = None):
     # Set isdir = True if the file is a directory!
-    f = File(scheme + '://' + path)
+    f = File(path)
     f.local_path = local_path
     f.path = path
+    f.scheme = scheme
     if netloc:
         f.netloc = netloc
     return f
