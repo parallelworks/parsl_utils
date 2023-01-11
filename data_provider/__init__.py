d9e4682bf062d10ff93801097cc3e5f67e8738d8
avidalto <avidalto@hawk.iit.edu>
2023-01-11 11:48:34 -0600

    fixing local_path

--- data_provider/__init__.py
+++ data_provider/__init__.py
@@ -1,16 +1,14 @@
-from parsl.data_provider.files import File as ParslFile
+from parsl.data_provider.files import File
 
 from . import rsync
 from . import gsutil
-from . import files
 
-def File(url, local_path):
-    """
-    Wrapper around Parsl native File object. The local_path (worker_path) 
-    attribute is lost in the Parsl native object so here this information
-    is added to the url
-    """
-    f = ParslFile(url + '#' + local_path)
-    # This information is lost along the way
+
+def PWFile(path, local_path, scheme, netloc):
+    if netloc.startswith('/'):
+        raise Exception('ERROR: netloc cannot start with /')
+
+    url = scheme + '://' + netloc + '/' + path
+    f = File(url)
     f.local_path = local_path
     return f
