4bcc20a57e08010e8dc5bdedb3d1cac3f5330dd4
avidalto <avidalto@hawk.iit.edu>
2023-01-11 11:19:50 -0600

    Create a wrapper around File

--- data_provider/__init__.py
+++ data_provider/__init__.py
@@ -1,15 +1,16 @@
-from parsl.data_provider.files import File
+from parsl.data_provider.files import File as ParslFile
 
 from . import rsync
 from . import gsutil
 from . import files
 
-
-def PWFile(path, local_path, scheme, netloc):
-    if netloc.startswith('/'):
-        raise Exception('ERROR: netloc cannot start with /')
-
-    url = scheme + '://' + netloc + '/' + path
-    f = File(url)
+def File(url, local_path):
+    """
+    Wrapper around Parsl native File object. The local_path (worker_path) 
+    attribute is lost in the Parsl native object so here this information
+    is added to the url
+    """
+    f = ParslFile(url + '#' + local_path)
+    # This information is lost along the way
     f.local_path = local_path
     return f
