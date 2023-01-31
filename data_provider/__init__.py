2b23e37c8398f3450946a416846c6e61985c5c17
avidalto <avidalto@hawk.iit.edu>
2023-01-31 16:23:01 -0600

    Fix relative paths

--- data_provider/__init__.py
+++ data_provider/__init__.py
@@ -1,3 +1,4 @@
+import os
 from parsl.data_provider.files import File
 
 from . import rsync
@@ -5,7 +6,15 @@ from . import gsutil
 
 
 def PWFile(url, local_path):
-    f = File(url + '#' + local_path)
+    if os.path.isabs(url):
+        f = File(url + '#' + local_path)
+    else:
+        f = File(
+            os.path.join(
+                os.getcwd(),
+                url
+            ) + '#' + local_path
+        )
     # This information is lost along the way
     f.local_path = local_path
     return f
