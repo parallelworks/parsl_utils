d3a6fe0b54620faa99bb70a2470080ea8945a928
avidalto <avidalto@hawk.iit.edu>
2023-02-03 13:45:32 -0600

    Improve logging

--- logs.py
+++ logs.py
@@ -1,13 +1,13 @@
 import logging
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+
 import os
 
-def get_logger(log_file, name):
+def get_logger(log_file, name, level = logging.INFO):
     os.makedirs(os.path.dirname(log_file), exist_ok = True)
-    
-    logging.basicConfig(
-        filename = log_file,
-        format = '%(asctime)s %(levelname)-8s %(message)s',
-        datefmt ='%Y-%m-%d %H:%M:%S',
-        level = logging.INFO
-    )
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
     return logging.getLogger(name)
