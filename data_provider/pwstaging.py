84ca0190a93b907123ec16f275a359344d68355d
avidalto <avidalto@hawk.iit.edu>
2023-03-24 15:04:08 -0500

    Improve log records

--- data_provider/pwstaging.py
+++ data_provider/pwstaging.py
@@ -96,7 +96,7 @@ class PWStaging(Staging, RepresentationMixin):
 def in_task_stage_in_cmd_wrapper(func, file, working_dir, cmd, cmd_id, log_level):
     def wrapper(*args, **kwargs):
         logger = get_logger(f'data_provider/{cmd_id}.log', cmd_id, level = log_level)
-        logger.info('Running command')
+        logger.info(f'Running command <{cmd}> with id <{cmd_id}>')
         if working_dir:
             os.makedirs(working_dir, exist_ok=True)
         
@@ -121,7 +121,7 @@ def in_task_stage_in_cmd_wrapper(func, file, working_dir, cmd, cmd_id, log_level
 def in_task_stage_out_cmd_wrapper(func, file, working_dir, cmd, cmd_id, log_level):
     def wrapper(*args, **kwargs):
         logger = get_logger(f'data_provider/{cmd_id}.log', cmd_id, level = log_level)
-        logger.info('Running command')
+        logger.info(f'Running command <{cmd}> with id <{cmd_id}>')
         logger.debug('Calling wrapped function')
         result = func(*args, **kwargs)
         logger.debug('Wrapped function returned')
