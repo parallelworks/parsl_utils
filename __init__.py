25b96c8a4b3c0261399fa5dea89bd5c5f5014bcd
avidalto <avidalto@hawk.iit.edu>
2023-06-23 16:19:29 -0500

    This file isno longer req

--- __init__.py
+++ __init__.py
@@ -5,7 +5,3 @@ from . import retry_handler
 
 if os.path.isfile('executors.json'):
     from . import config
-
-if 'PARSL_CLIENT_HOST' in os.environ:
-    # Only works if we are running parsl_utils in the user container
-    from . import resource_info
