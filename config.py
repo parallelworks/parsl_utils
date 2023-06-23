7628c8f01e127eff6cbabf1ab08fb5111e900936
avidalto <avidalto@hawk.iit.edu>
2023-06-23 15:25:03 -0500

    Update config to get data from form

--- config.py
+++ config.py
@@ -16,57 +16,52 @@ from parsl_utils.data_provider.rsync import PWRSyncStaging
 from parsl_utils.data_provider.gsutil import PWGsutil
 from parsl_utils.data_provider.s3 import PWS3
 
-def read_args():
-    parser = argparse.ArgumentParser()
-    parsed, unknown = parser.parse_known_args()
-    for arg in unknown:
-        if arg.startswith(("-", "--")):
-            parser.add_argument(arg, default="", nargs="?")
-    pwargs = vars(parser.parse_args())
-    return pwargs
 
-pwargs = read_args()
+def get_provider_parameters_from_form(resource_inputs):
+    provider_options = {}
+    for k,v in inputs_dict.items():
+        if k.startswith('_parsl_provider_'):
+            key = k.replace('_parsl_provider_', '')
+            provider_options[key] = v
+
+    return provider_options
+
+
+# Find all resource labels
+with open('inputs.json') as inputs_json:
+    inputs_dict = json.load(inputs_json)
+        
+resource_labels = [label.replace('pwrl_','') for label in inputs_dict.keys() if label.startswith('pwrl_')]    
 
 # Need to name the job to be able to remove it with clean_resources.sh!
-job_number = os.getcwd().split('/')[-1]
-
-with open('executors.json', 'r') as f:
-    exec_conf = json.load(f)
-
-for label, executor in exec_conf.items():
-    for k, v in executor.items():
-        if type(v) == str:
-            exec_conf[label][k] = os.path.expanduser(v)
+job_name = '-'.join(os.getcwd().split('/')[-2:])
 
 # Define HighThroughputExecutors
 executors = []
-for exec_label, exec_conf_i in exec_conf.items():
+for label in resource_labels:
+    resource_inputs_json = os.path.join('resources', label, 'inputs.json')
+    with open(resource_inputs_json) as inputs_json:
+        resource_inputs = json.load(inputs_json)
     
-    # Set default values:
-    if 'SSH_CHANNEL_SCRIPT_DIR' not in exec_conf_i:
-        script_dir = os.path.join(exec_conf_i['RUN_DIR'], 'ssh_channel_script_dir')
-    else:
-        script_dir = exec_conf_i['SSH_CHANNEL_SCRIPT_DIR']
+    script_dir = os.path.join(resource_inputs['resource']['jobdir'], 'ssh_channel_script_dir')
+    worker_logdir_root = os.path.join(resource_inputs['resource']['jobdir'], 'worker_logdir_root')
     
-    if 'WORKER_LOGDIR_ROOT' not in exec_conf_i:
-        worker_logdir_root = os.path.join(exec_conf_i['RUN_DIR'], 'worker_logdir_root')
-    else:
-        worker_logdir_root =  exec_conf_i['WORKER_LOGDIR_ROOT']
-
     # To support kerberos:
     gssapi_auth = False
-    if 'GSSAPI_AUTH' in exec_conf_i:
-        if exec_conf_i['GSSAPI_AUTH'] == "true":
+    if 'gssapi_auth' in resource_inputs:
+        if resource_inputs['gssapi_auth'] == "true":
             gssapi_auth = True
 
+    hostname = resource_inputs['resource']['publicIp']
+    if '@' in hostname:
+        hostname = hostname.split('@')[1]
+
     channel = SSHChannel(
-        hostname = exec_conf_i['HOST_IP'],
-        username = exec_conf_i['HOST_USER'],
+        hostname = hostname,
+        username = resource_inputs['resource']['username'],
         # Full path to a script dir where generated scripts could be sent to
         script_dir = script_dir,
-            key_filename = '/home/{PW_USER}/.ssh/pw_id_rsa'.format(
-            PW_USER =  exec_conf_i['HOST_USER']
-        ),
+        key_filename = os.path.expanduser('~/.ssh/pw_id_rsa'),
         gssapi_auth = gssapi_auth
     )
     
@@ -74,7 +69,7 @@ for exec_label, exec_conf_i in exec_conf.items():
     # SSHChannel hostname and port with
     # localhost and local port to which
     # cluster login node 22 is forwarded.
-    ssh_jump_config='/tmp/.ssh/'+exec_conf_i['POOL']+'.config'
+    ssh_jump_config = os.path.join('/tmp/.ssh/', resource_inputs['resource']['name'], '.config')
     if os.path.isfile(ssh_jump_config):
         # There is a jump box. Get localport.
         file = open(ssh_jump_config, 'r')
@@ -83,49 +78,51 @@ for exec_label, exec_conf_i in exec_conf.items():
             if "Port" in line:
                 localport=line.split()[1]
 
-	# Adjust Parsl config
-        channel.hostname="localhost"
-        channel.port=localport
+	    # Adjust Parsl config
+        channel.hostname = "localhost"
+        channel.port = localport
 
     # Define worker init:
     # - export PYTHONPATH={run_dir} is needed to use custom staging providers
     worker_init = 'export PYTHONPATH={run_dir}; bash {workdir}/pw/remote.sh; bash {workdir}/pw/.pw/remote.sh; source {conda_sh}; conda activate {conda_env}; cd {run_dir}; {clean_cmd}'.format(
-        workdir = exec_conf_i['WORKDIR'],
-        conda_sh = os.path.join(exec_conf_i['CONDA_DIR'], 'etc/profile.d/conda.sh'),
-        conda_env = exec_conf_i['CONDA_ENV'],
-        run_dir = exec_conf_i['RUN_DIR'],
+        workdir = resource_inputs['resource']['workdir'],
+        conda_sh = os.path.join(resource_inputs['worker_conda_dir'], 'etc/profile.d/conda.sh'),
+        conda_env = resource_inputs['worker_conda_env'],
+        run_dir = resource_inputs['resource']['jobdir'],
         clean_cmd = "ps -x | grep worker.pl | grep -v grep | awk '{print $1}'"
     )
 
     # Data provider:
     # One instance per executor
     storage_access = [ 
-        PWRSyncStaging(exec_label),
-        PWGsutil(exec_label),
-        PWS3(exec_label)
+        PWRSyncStaging(label),
+        PWGsutil(label),
+        PWS3(label)
     ]
 
     # Define provider
-    if 'PBSProProvider' in exec_conf_i:
+    provider_options = get_provider_parameters_from_form(resource_inputs)
+    
+    if resource_inputs['jobschedulertype'] == 'PBS':
         provider = PBSProProvider(
-            **json.loads(exec_conf_i['PBSProProvider']),
+            **provider_options,
             worker_init = worker_init,
             channel = channel
-
         )
 
-    elif 'SlurmProvider' in exec_conf_i:
+    elif resource_inputs['jobschedulertype'] = 'SLURM':
         provider = SlurmProvider(
-            **json.loads(exec_conf_i['SlurmProvider']),
+            **provider_options,
             worker_init = worker_init,
             channel = channel
         )
+
     else:
         # Need to overwrite the default worker_init since we don't want to run remote.sh in this case
         worker_init = 'export PYTHONPATH={run_dir}; source {conda_sh}; conda activate {conda_env}; cd {run_dir}'.format(
-            conda_sh = os.path.join(exec_conf_i['CONDA_DIR'], 'etc/profile.d/conda.sh'),
-            conda_env = exec_conf_i['CONDA_ENV'],
-            run_dir = exec_conf_i['RUN_DIR']
+            conda_sh = os.path.join(resource_inputs['worker_conda_dir'], 'etc/profile.d/conda.sh'),
+            conda_env = resource_inputs['worker_conda_env'],
+            run_dir = resource_inputs['resource']['jobdir']
         )
 
         provider = LocalProvider(
@@ -133,10 +130,8 @@ for exec_label, exec_conf_i in exec_conf.items():
             channel = channel
         )
 
-    if 'cores_per_worker' in exec_conf_i:
-        cores_per_worker = float(exec_conf_i['cores_per_worker'])
-    elif 'CORES_PER_WORKER' in exec_conf_i:
-        cores_per_worker = float(exec_conf_i['CORES_PER_WORKER'])
+    if 'cores_per_worker' in resource_inputs:
+        cores_per_worker = float(resource_inputs['cores_per_worker'])
     else:
         cores_per_worker = 1.0
 
@@ -144,22 +139,22 @@ for exec_label, exec_conf_i in exec_conf.items():
     executors.append(
         HighThroughputExecutor(
             worker_ports=((
-                int(exec_conf_i['WORKER_PORT_1']), 
-                int(exec_conf_i['WORKER_PORT_2'])
+                resource_inputs['resource']['ports'][0], 
+                resource_inputs['resource']['ports'][1]
             )),
-            label = exec_label,
+            label = label,
             worker_debug = True,             # Default False for shorter logs
-            working_dir =  exec_conf_i['RUN_DIR'],
+            working_dir =  resource_inputs['resource']['jobdir'],
             cores_per_worker = cores_per_worker,
             worker_logdir_root = worker_logdir_root,
-            address = exec_conf_i['ADDRESS'],
+            address = resource_inputs['resource']['privateIp'],
             provider = provider,
             storage_access = storage_access
         )
     )
     
-if 'parsl_retries' in pwargs:
-    retries = int(pwargs['parsl_retries'])
+if 'parsl_retries' in inputs_dict:
+    retries = int(inputs_dict['parsl_retries'])
     from . import retry_handler
     retry_handler = retry_handler.retry_handler
 else:
@@ -174,7 +169,7 @@ if len(executors) > 1:
     )
 else:
     from parsl.monitoring.monitoring import MonitoringHub
-    executor_address = list(exec_conf.values())[0]['ADDRESS']
+    executor_address = resource_inputs['resource']['privateIp']
     config = Config(
         retries = retries,
         retry_handler = retry_handler,
@@ -182,7 +177,7 @@ else:
         monitoring = MonitoringHub(
             hub_address = executor_address,
             monitoring_debug = False,
-            workflow_name = str(job_number),
+            workflow_name = str(job_name),
             logging_endpoint = 'sqlite:////pw/.monitoring.db',
             resource_monitoring_enabled = False,
         )
