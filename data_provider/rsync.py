cff89e0f9c254293dee2d5f83740cd962f8e8be2
stefangary <40672572+stefangary@users.noreply.github.com>
2023-10-24 12:54:34 -0400

    Pass head node private IP directly to PWRsyncStaging
    This allows the user to explicitly specify the IP address used with rsync in the Parsl configuration instead of **assuming** that this information is present in the launch_cmd.

--- data_provider/rsync.py
+++ data_provider/rsync.py
@@ -38,13 +38,14 @@ def get_stage_out_cmd(file, jumphost = None):
 
     return cmd
 
-class PWRSyncStaging(pwstaging.PWStaging):
+class PWRSyncStaging(pwstaging.PWStaging, head_node_private_ip):
     """
     This is a modification of the official staging provider 
     https://parsl.readthedocs.io/en/latest/stubs/parsl.data_provider.rsync.RSyncStaging.html
-    with two changes:
+    with three changes:
         1. Add -avzq option to rsync
         2. Make parent directory of file.path if it does not exist
+        3. Allow the user the specify the private IP of the head node.
 
     This staging provider will execute rsync on worker nodes
     to stage in files from a remote location.
@@ -53,6 +54,14 @@ class PWRSyncStaging(pwstaging.PWStaging):
     initialization could include an appropriate SSH key configuration.
     The submit side will need to run an rsync-compatible server (for example,
     an ssh server with the rsync binary installed)
+
+    It is assumed that the SSH tunnels/config from the PW platform
+    are already in place so that worker nodes can simply
+         ssh -J <head_node_private_ip> usercontainer
+    in order to reach the PW user's IDE. All rsync commands here are
+    prefixed with ssh -J and this approach works even if the Parsl
+    app is for some reason running on the head node itself instead
+    of the worker nodes.
     """
 
     def __init__(self, executor_label, logging_level = logging.INFO):
@@ -62,19 +71,13 @@ class PWRSyncStaging(pwstaging.PWStaging):
 
     def replace_task(self, dm, executor, file, f):
         working_dir = dm.dfk.executors[executor].working_dir
-        #cmd = get_stage_in_cmd(file, jumphost = dm.dfk.executors[executor].address)
-        # Get private IP address of head node from the launch_cmd
-        # Need to parse the -a {address} option (so the address itself is
-        # first after the split) and then split again to discard the rest of the options.
-        cmd = get_stage_in_cmd(file, jumphost = dm.dfk.executors[executor].launch_cmd.split('-a')[1].split('-')[0])
+        cmd = get_stage_in_cmd(file, jumphost = head_node_private_ip)
         cmd_id = self._get_cmd_id(cmd)  
         return pwstaging.in_task_stage_in_cmd_wrapper(f, file, working_dir, cmd, cmd_id, self.logger.getEffectiveLevel())
     
     def replace_task_stage_out(self, dm, executor, file, f):
         working_dir = dm.dfk.executors[executor].working_dir
-        #cmd = get_stage_out_cmd(file, jumphost = dm.dfk.executors[executor].address)
-        # See comments in replace_task, above.
-        cmd = get_stage_out_cmd(file, jumphost = dm.dfk.executors[executor].launch_cmd.split('-a')[1].split('-')[0])
+        cmd = get_stage_out_cmd(file, jumphost = head_node_private_ip)
         cmd_id = self._get_cmd_id(cmd)  
         cmd_id = self._get_cmd_id(cmd)  
         return pwstaging.in_task_stage_out_cmd_wrapper(f, file, working_dir, cmd, cmd_id, self.logger.getEffectiveLevel())
